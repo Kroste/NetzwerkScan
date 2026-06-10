c757cd53cb278a253382daee2d0318f2e057a394
Lars Oste <lars-oste@gmx.de>
2026-06-10 19:46:53 +0200

    Layout Aboutbox an MainView angepasst

--- Views/AboutWindow.axaml.cs
+++ Views/AboutWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -5,8 +6,37 @@ namespace NetScanner.Views;
 
 public partial class AboutWindow : Window
 {
-    public AboutWindow() => InitializeComponent();
+    public AboutWindow()
+    {
+        InitializeComponent();
+        UpdateChrome(WindowState);
+    }
 
+    // Avalonia 12: kein Subscribe(Action<T>) ohne System.Reactive -> Property-Override nutzen.
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == WindowStateProperty)
+            UpdateChrome(WindowState);
+    }
+
+    /// <summary>Bei maximiertem Fenster oben Luft lassen (Custom-Chrome) und Max/Restore-Glyph umschalten.</summary>
+    private void UpdateChrome(WindowState state)
+    {
+        Padding = state == WindowState.Maximized ? new Thickness(7) : new Thickness(0);
+        if (this.FindControl<Control>("MaxGlyph") is { } max)
+            max.IsVisible = state != WindowState.Maximized;
+        if (this.FindControl<Control>("RestoreGlyph") is { } restore)
+            restore.IsVisible = state == WindowState.Maximized;
+    }
+
+    // --- Fenster-Buttons ---
+    private void OnMinimize(object? sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    private void OnMaximizeRestore(object? sender, RoutedEventArgs e) =>
+        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    private void OnClose(object? sender, RoutedEventArgs e) => Close();
+
+    // --- Inhalt ---
     private async void OnCoffeeClick(object? sender, RoutedEventArgs e)
         => await OpenUrlAsync("https://buymeacoffee.com/kroste");
 
